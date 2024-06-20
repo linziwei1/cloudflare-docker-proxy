0e9d1510caf2a21d89423340c9844edf3eb55316
linziwei1 <1186362656@qq.com>
2024-06-20 15:46:12 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,13 +7,7 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.linjoey.cn": "https://registry-1.docker.io",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
